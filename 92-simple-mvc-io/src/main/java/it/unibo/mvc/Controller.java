22ac4a547c7307a81f39c2fc27027bff9a3a3e22
Nicolas Montanari <nicolas.montanari3@studio.unibo.it>
2024-11-18 12:20:49 +0100

    ControllerDone

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,37 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File currentFile = new File(System.getProperty("user.home"), "output.txt");
 
+    public void setFile(final File file){
+        if(file.exists() && file.isFile()){
+            currentFile = file;
+        }else{
+            throw new IllegalArgumentException("File not exist");
+        }
+    }
+
+    public File getCurrentFile(){
+        return currentFile;
+    }
+
+    public String getPath(){
+        return currentFile.getAbsolutePath();
+    }
+
+    public void writeString(final String string){
+        try (PrintStream ps = new PrintStream(getPath(), StandardCharsets.UTF_8)) {
+            ps.print(string);
+        } catch (IOException e1) {
+            throw new IllegalArgumentException("error print file");
+        }
+    }
 }
